0a36014bee5f3c6a924ea2df61d7debe878730b8
Márcio Ribeiro de Oliveira <marcio035@gmail.com>
2026-04-02 16:51:15 -0400

    Configurando karpenter para o cluster control plane e ajustando node group

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "eks_control_plane" {
     helm    = helm.control_plane
   }
 
-  source                   = "github.com/marciorbr/terraform-aws-eks.git?ref=v1.4.2"
+  source                   = "github.com/marciorbr/terraform-aws-eks.git?ref=v1.4.3"
   k8s_version              = local.k8s_version_control_plane
   project_name             = local.project_name_control_plane
   environment              = local.environment
@@ -25,11 +25,24 @@ module "eks_control_plane" {
   vpc_id                   = module.vpc.vpc_id
   on_demand_instance_types = ["t3.medium", "t3a.medium"]
   on_demand_auto_scale_options = {
-    desired = 2
-    max     = 2
-    min     = 1
+    desired = 3
+    max     = 4
+    min     = 3
   }
   addons = ["eks-pod-identity-agent", "vpc-cni", "kube-proxy", "coredns"]
+
+  karpenter_capacity = [
+    {
+      name               = "loki"
+      workload           = "loki"
+      ami_family         = "AL2023"
+      ami_ssm            = "/aws/service/eks/optimized-ami/1.34/amazon-linux-2023/x86_64/standard/recommended/image_id"
+      instance_family    = ["t3", "t3a"]
+      instance_sizes     = ["small", "medium", "large"]
+      capacity_type      = ["spot", "on-demand"]
+      availability_zones = ["us-east-1a", "us-east-1b"]
+    }
+  ]
 
   enable_aws_lb_controller = true
 
@@ -45,7 +58,7 @@ module "eks_worker_01" {
     helm    = helm.worker_01
   }
 
-  source                   = "github.com/marciorbr/terraform-aws-eks.git?ref=v1.4.2"
+  source                   = "github.com/marciorbr/terraform-aws-eks.git?ref=v1.4.3"
   k8s_version              = local.k8s_version_worker_01
   project_name             = local.project_name_worker_01
   environment              = local.environment
@@ -54,7 +67,7 @@ module "eks_worker_01" {
   vpc_id                   = module.vpc.vpc_id
   on_demand_instance_types = ["t3.medium", "t3a.medium"]
   on_demand_auto_scale_options = {
-    desired = 2
+    desired = 1
     max     = 2
     min     = 1
   }
@@ -83,10 +96,10 @@ module "eks_worker_01" {
 
   karpenter_capacity = [
     {
-      name               = "karpenter-dev"
+      name               = "dev"
       workload           = "dev"
       ami_family         = "AL2023"
-      ami_ssm            = "/aws/service/eks/optimized-ami/1.33/amazon-linux-2023/x86_64/standard/recommended/image_id"
+      ami_ssm            = "/aws/service/eks/optimized-ami/1.34/amazon-linux-2023/x86_64/standard/recommended/image_id"
       instance_family    = ["t3", "t3a"]
       instance_sizes     = ["small", "medium", "large"]
       capacity_type      = ["spot"]
