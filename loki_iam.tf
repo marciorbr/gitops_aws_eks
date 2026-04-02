62b73907af87f8686427e81dd2c5e445063b1e2d
Márcio Ribeiro de Oliveira <marcio035@gmail.com>
2026-04-02 16:52:12 -0400

    Configurando IAM para o loki

---
+++ loki_iam.tf
@@ -0,0 +1,67 @@
+data "aws_iam_policy_document" "loki_role" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
+
+resource "aws_iam_role" "loki_role" {
+  assume_role_policy = data.aws_iam_policy_document.loki_role.json
+  name               = "eks-loki-role-${local.project_name_control_plane}"
+}
+
+data "aws_iam_policy_document" "loki_policy" {
+  version = "2012-10-17"
+
+  statement {
+
+    effect = "Allow"
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      format("%s/*", aws_s3_bucket.loki-chunks.arn),
+      format("%s/*", aws_s3_bucket.loki-admin.arn),
+      format("%s/*", aws_s3_bucket.loki-ruler.arn),
+      aws_s3_bucket.loki-chunks.arn,
+      aws_s3_bucket.loki-admin.arn,
+      aws_s3_bucket.loki-ruler.arn,
+    ]
+
+  }
+}
+
+resource "aws_iam_policy" "loki_policy" {
+  name        = "eks-loki-policy-${local.project_name_control_plane}"
+  path        = "/"
+  description = local.project_name_control_plane
+  policy = data.aws_iam_policy_document.loki_policy.json
+}
+
+resource "aws_iam_policy_attachment" "loki" {
+  name = "loki"
+  roles = [
+    aws_iam_role.loki_role.name
+  ]
+
+  policy_arn = aws_iam_policy.loki_policy.arn
+}
+
+resource "aws_eks_pod_identity_association" "loki" {
+  cluster_name    = module.eks_control_plane.eks_cluster_name
+  namespace       = "loki"
+  service_account = "loki"
+  role_arn        = aws_iam_role.loki_role.arn
+}
